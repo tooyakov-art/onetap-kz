0a1ec72ab2793116db9f1fcc03e8a21dab886ad5
Dias Tuyakov <tooyakov@gmail.com>
2026-08-19 12:26:21 +0500

    Use extended screenshot driver

--- test_driver/integration_test.dart
+++ test_driver/integration_test.dart
@@ -1,6 +1,6 @@
 import 'dart:io';
 
-import 'package:integration_test/integration_test_driver.dart';
+import 'package:integration_test/integration_test_driver_extended.dart';
 
 Future<void> main() async {
   await integrationDriver(
